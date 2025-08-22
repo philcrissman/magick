aab1fa98c0fbc1fe6b77095a2aa2ffd5255dd827
Phil Crissman <phil.crissman@gmail.com>
2025-08-22 08:06:45 -0700

    Added a power function
    Power is based on power from EoP (Stepanov, McJones) and uses the
    fast exponentiation algorithm to compute powers of a function. Any
    associative function can be used, so passing `add` makes power compute
    multiplication, and passing `multiply` makes power compute
    exponentiation.
    
    Multiply's implementation was changed to use `power` instead of
    recursively adding elements, subtracting only 1 each time.
    
    `exp` function was added to compute exponents.

--- lib/magick/maths.rb
+++ lib/magick/maths.rb
@@ -5,6 +5,69 @@ module Magick
   module Maths
     class << self
       def add = ->(x){ ->(y) { x + y }}
+
+      def negate = ->(x) { -x }
+
+      def subtract = ->(x){ ->(y) { add.(x).(negate.(y)) }}
+
+      def multiply
+        ->(a){
+          ->(b){
+            power.(a).(b).(add)
+          }
+        }
+      end
+      alias :mult :multiply
+
+      def divide
+        Smullyan::Birds::Y.(->(f){ 
+          ->(n){ 
+            ->(d){ 
+              n < d ? 0 : 1 + f.(n - d).(d)
+            }
+          }
+        })
+      end
+
+      def mod
+        Smullyan::Birds::Y.(->(f){
+          ->(n){
+            ->(d){
+              n < d ? n : f.(n - d).(d)
+            }
+          }
+        })
+      end
+
+      def exp
+        ->(a){
+          ->(b){
+            power.(a).(b).(multiply)
+          }
+        }
+      end
+
+      def power
+        # power needs a base, an exponent, and an
+        # operation. Powers of addition is equivalent
+        # to multiplication; powers of multiplication
+        # is equivalent to exponentiation
+        ->(base) { 
+          ->(exponent) {
+            ->(op) {
+              Smullyan::Birds::Y.(->(f) {
+                ->(b) {
+                  ->(e) {
+                    e == 1 ? b :
+                    mod.(e).(2) == 0 ? f.(op.(b).(b)).(divide.(e).(2)) :
+                    op.(f.(op.(b).(b)).(divide.(e).(2))).(b)
+                  }
+                }
+              }).(base).(exponent)
+            }
+          }
+        }
+      end
     end
   end
 end
--- test/test_maths.rb
+++ test/test_maths.rb
@@ -6,4 +6,40 @@ class TestMaths < Minitest::Test
   def test_add
     assert_equal 4, Magick::Maths::add.(2).(2)
   end
+
+  def test_negate
+    assert_equal (-2), Magick::Maths::negate.(2)
+    assert_equal 2, Magick::Maths::negate.(-2)
+  end
+
+  def test_subtract
+    assert_equal 2, Magick::Maths::subtract.(4).(2)
+  end
+
+  def test_multiply
+    assert_equal 12, Magick::Maths::multiply.(4).(3)
+  end
+
+  def test_mult
+    assert_equal 12, Magick::Maths::mult.(4).(3)
+  end
+
+  def test_divide
+    assert_equal 3, Magick::Maths::divide.(12).(4)
+  end
+
+  def test_mod
+    assert_equal 0, Magick::Maths::mod.(12).(3)
+    assert_equal 1, Magick::Maths::mod.(11).(2)
+  end
+
+  def test_exp
+    assert_equal 16, Magick::Maths::exp.(2).(4)
+    assert_equal 32, Magick::Maths::exp.(2).(5)
+  end
+
+  def test_power
+    assert_equal 9, Magick::Maths::power.(3).(3).(Magick::Maths::add)
+    assert_equal 27, Magick::Maths::power.(3).(3).(Magick::Maths::multiply)
+  end
 end
